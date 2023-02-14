3d264997ecd4ed6e627d58fb39ae40665977e4bd
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-02-13 21:55:47 -0500

    product grid and item widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/products_overview_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,32 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My Shop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Shop'),
-      ),
-      body: const Center(
-        child: Text("Let's build a shop!"),
-      ),
+      home: ProductsOverviewPage(),
     );
   }
 }
